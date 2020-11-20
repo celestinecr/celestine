fc8a2d3396ccf1aa861ae70b506cb63cdb105a86
Ian Rash <ian@sol.vin>
2020-11-19 17:52:01 -0800

    Adding some basic tests

--- spec/celestine_spec.cr
+++ spec/celestine_spec.cr
@@ -1,9 +1,11 @@
 require "./spec_helper"
 
-describe Celestine do
-  # TODO: Write tests
+module Celestine::Test
+  BLANK_SVG = %Q[<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"><defs></defs></svg>]
+end
 
-  it "works" do
-    false.should eq(true)
+describe Celestine do
+  it "should generate a blank SVG document" do
+    Celestine.draw { |ctx| }.should eq(Celestine::Test::BLANK_SVG)
   end
 end
