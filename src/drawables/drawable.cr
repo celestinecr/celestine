bdb34faba505e56287571e965f8fb809a1b0a7b0
Ian Rash <ian@sol.vin>
2020-11-21 21:51:10 -0800

    Fix custom attrs issue with formatting

--- src/drawables/drawable.cr
+++ src/drawables/drawable.cr
@@ -82,7 +82,7 @@ abstract struct Celestine::Drawable
   # Rendered custom attributes
   def custom_attribute(io : IO)
     # TODO: Fix this to work with SIFNumber and properly use quotes when appropriate
-    custom_attrs.keys.map { |k| %Q[#{k}="#{custom_attrs[k]}"] }.join(io, " ")
+    "#{custom_attrs.keys.map { |k| %Q[#{k}="#{custom_attrs[k]}"] }.join(io, " ")} "
   end
 
   abstract def draw(io : IO) : Nil 
