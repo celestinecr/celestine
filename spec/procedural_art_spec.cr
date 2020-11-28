b509ed1b804e1a2a3b10df3ca73dbf5eca901d63
Ian Rash <ian@sol.vin>
2020-11-28 05:12:04 -0800

    Fix issues

---
+++ spec/procedural_art_spec.cr
@@ -0,0 +1,39 @@
+describe ProceduralArt do
+  it "should make 100 hypnos images" do
+    100.times do
+      ProceduralArt::Hypnos.make
+    end
+  end
+
+  it "should make 10 inward images" do
+    10.times do
+      ProceduralArt::Inward.make
+      ProceduralArt::Inward.seed += 1
+    end
+    ProceduralArt::Inward.seed = 1
+  end
+
+  it "should make 10 mineshift-s images" do
+    10.times do
+      ProceduralArt::Mineshift::Simple.make
+      ProceduralArt::Mineshift::Simple.seed += 1
+    end
+    ProceduralArt::Mineshift::Simple.seed = 1
+  end
+
+  it "should make 10 chromatic images" do
+    10.times do
+      ProceduralArt::ChromaticAberration.make
+      ProceduralArt::ChromaticAberration.seed += 1
+    end
+    ProceduralArt::ChromaticAberration.seed = 1
+  end
+
+  it "should make 10 patchwork images" do
+    10.times do
+      ProceduralArt::SemiCirclePatchwork.make
+      ProceduralArt::SemiCirclePatchwork.seed += 1
+    end
+    ProceduralArt::SemiCirclePatchwork.seed = 1
+  end
+end
