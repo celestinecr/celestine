0f79c6d10df607f63bc8e123cfa972b9c9f74d27
Solomon Jackson <sugarfi@sugarfi.dev>
2020-12-14 16:01:58 -0500

    fix minor bug in rectangle spec, add all the animateTransform types

---
+++ src/effects/animation/transform/scale.cr
@@ -0,0 +1,44 @@
+class Celestine::Animate::Transform::Scale < Celestine::Drawable
+  TAG = "animateTransform"
+  property repeat_count : IFNumber | String? = nil
+  make_units duration
+  make_units repeat_duration
+
+  #TODO: Can I use these with units?
+  property? use_from = false
+  property? use_to = false
+  property? use_by = false
+
+  property from_x : Float64? = nil
+  property to_x : Float64? = nil
+  property by_x : Float64? = nil
+
+  property from_y : Float64? = nil
+  property to_y : Float64? = nil
+  property by_y : Float64? = nil
+
+  def draw(io : IO) : Nil
+    io << %Q[<#{TAG} ]
+    # Puncuate attributes with a space 
+    draw_attributes(io)
+
+    io << %Q[attributeName="scale" ]
+    io << %Q[attributeType="XML" ]
+    io << %Q[type="translate" ]
+    io << %Q[repeatCount="#{repeat_count}" ]                                 if repeat_count
+    io << %Q[repeatDur="#{repeat_duration}#{repeat_duration_units}" ]        if repeat_duration
+    io << %Q[dur="#{duration}#{duration_units}" ]                            if duration
+    io << %Q[from="#{from_x} #{from_y}" ]                                    if use_from?               
+    io << %Q[to="#{to_x} #{to_y}" ]                                          if use_to?              
+    io << %Q[by="#{by_x} #{by_y}" ]                                          if use_by?                   
+
+    if inner_elements.empty?
+      io << %Q[/>]
+    else
+      io << ">"
+      io << inner_elements
+      io << "</#{TAG}>"
+    end
+
+  end
+end
---
+++ src/effects/animation/transform/skew_y.cr
@@ -0,0 +1,40 @@
+class Celestine::Animate::Transform::SkewY < Celestine::Drawable
+  TAG = "animateTransform"
+  property repeat_count : IFNumber | String? = nil
+  make_units duration
+  make_units repeat_duration
+
+  #TODO: Can I use these with units?
+  property? use_from = false
+  property? use_to = false
+  property? use_by = false
+
+  property from : Float64? = nil
+  property to : Float64? = nil
+  property by : Float64? = nil
+
+  def draw(io : IO) : Nil
+    io << %Q[<#{TAG} ]
+    # Puncuate attributes with a space 
+    draw_attributes(io)
+
+    io << %Q[attributeName="skewY" ]
+    io << %Q[attributeType="XML" ]
+    io << %Q[type="translate" ]
+    io << %Q[repeatCount="#{repeat_count}" ]                                 if repeat_count
+    io << %Q[repeatDur="#{repeat_duration}#{repeat_duration_units}" ]        if repeat_duration
+    io << %Q[dur="#{duration}#{duration_units}" ]                            if duration
+    io << %Q[from="#{from}" ]                                                if use_from?               
+    io << %Q[to="#{to}" ]                                                    if use_to?              
+    io << %Q[by="#{by}" ]                                                    if use_by?                   
+
+    if inner_elements.empty?
+      io << %Q[/>]
+    else
+      io << ">"
+      io << inner_elements
+      io << "</#{TAG}>"
+    end
+
+  end
+end
